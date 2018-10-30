9b19710d2b16242d7f161026d23f511fdba2da36
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-30 20:34:20 +0000

    Working on save method

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+
+  end
+
+  def self.save(id, name, type)
+    @db.execute("INSERT INTO pokemon(id, name, type) VALUES (?, ?, ?)", id, name, type)
+  end
+
 end
