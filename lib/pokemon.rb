0ed5a7035d908596242b3bea08fa508ec0fc755c
Diana Badas <diana.badas@gmail.com>
2019-03-04 12:33:47 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
 class Pokemon
+
+  @@all = []
+
+  attr_reader :id
+  attr_accessor :name, :type, :db
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon(name, type) VALUES (?, ?);",name, type)
+  end
+
+  def self.find(id, db)
+    db.execute("SELECT * FROM pokemon WHERE id = (?);",id)
+
+    error porque no existe. si no existe crear
+  end
+
 end
