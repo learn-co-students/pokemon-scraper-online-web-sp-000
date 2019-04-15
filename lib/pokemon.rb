dc0becbcbdf5edc4f667f0b054ca8f2ee6b0d7f2
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-15 20:22:01 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  @@all = []
+
+  def initialize (id, name, type, db)
+    self.id = id
+    self.name = name
+    self.type = type
+    self.db = db
+  end
+
+  def self.save(name, type, db)
+  end
+
+  def self.find(id, db)
+    #db.execute("SELECT * FROM pokemon WHERE id == #{id};")
+  end
 end
