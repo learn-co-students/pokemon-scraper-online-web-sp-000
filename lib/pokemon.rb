79a63d1791042c183b7661da60f5718c4b04c3da
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-23 22:57:04 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,47 @@
+require 'pry'
+
 class Pokemon
+  attr_accessor :name, :type, :db, :hp
+  attr_reader :id
+
+  def initialize(id: nil, name:, type:, hp: nil, db:)
+    @name = name
+    @type = type
+    @id = id
+    @hp = nil
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+    SQL
+
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+    SQL
+
+    result = db.execute(sql, id)[0]
+    id = result[0]
+    name = result[1]
+    type = result[2]
+    hp = result[3]
+# binding.pry
+    pokemon = Pokemon.new(id: id, name: name, type: type, hp: hp, db: db)
+  end
+
+  def alter_hp(hp, db)
+    sql = "UPDATE pokemon SET hp = ? WHERE id = ?"
+    binding.pry
+    db.execute(sql, hp, self.id)
+    # binding.pry
+    self.hp = hp
+  end
 end
