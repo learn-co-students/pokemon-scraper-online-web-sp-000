e460450a403eeb2921610b00b6efa12c20324e40
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-06 01:00:52 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,28 +1,33 @@
-
-class Pokemon
-  attr_accessor :name, :id, :type, :id, :db, :hp
-  @@all = []
-
-  def initialize(pokemon)
-    @@all  << self
-  end
-
-  def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", [name], [type])
-  end
-
-  def self.find(id, db)
-    pokemon = db.execute("SELECT * FROM pokemon WHERE id=?;", [id])
-    newpokemon = self.new(pokemon)
-    newpokemon.id = pokemon[0][0]
-    newpokemon.name = pokemon[0][1]
-    newpokemon.type = pokemon[0][2]
-    newpokemon.hp = pokemon[0][3]
-    newpokemon
-  end
-
-  def alter_hp(num, db)
-    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [num], [self.id])
-    self.hp = num
-  end
-end
+	class Pokemon
+	
+	attr_accessor :id, :name, :type, :db, :hp
+	@@all = []
+	
+	  def initialize(name:, type:, id:, db:, hp: nil)
+	    @name = name
+	    @type = type
+	    @id = id
+	    @hp = hp
+	    @db = db
+	    @@all << self 
+	  end
+	
+	  def self.save(name, type, db)
+	    db.execute("INSERT INTO pokemon(name, type) VALUES (?,?)", name, type)
+	  end
+	
+	  def self.find(id_number, db)
+	    pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id=?", id_number).first
+	    Pokemon.new(id: pokemon_from_db[0], 
+	              name: pokemon_from_db[1], 
+	              type: pokemon_from_db[2], 
+	              hp: pokemon_from_db[3], 
+	              db: db )
+	  end
+	
+	  def alter_hp(new_hp, db)
+	    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+	  end
+	
+	
+	end
