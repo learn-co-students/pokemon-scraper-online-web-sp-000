f3e8986737d84bbb7b0183e3ceba501d2a2d3cea
alex6851 <alex.baker685@gmail.com>
2019-06-06 22:12:47 -0700

    initial commit

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,35 @@
+require 'pry'
+
 class Pokemon
+    attr_accessor :name, :type, :id, :db
+
+def initialize(id:nil,name:,type:, db:)
+    @name = name
+    @type = type
+    @id = id
+    @db = db
+    binding.pry
+end
+
+def self.save
+    binding.pry
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+      SQL
+      binding.pry
+      @db.execute(sql, self.name, self.type)
+      @id = @db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+end
+
+  def update
+    binding.pry
+    sql = "UPDATE pokemon SET name = ?, type = ? WHERE id = ?"
+    @db.execute(sql, self.name, self.type, self.id)
+  end
+
 end
