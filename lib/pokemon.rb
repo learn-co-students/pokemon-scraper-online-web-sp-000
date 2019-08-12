449b2bc49bce1ac15756c9b0fd00069359673d0e
Rick Rosetti <rick.rosetti@gmail.com>
2019-08-12 13:23:54 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
 class Pokemon
+
+attr_accessor :name, :type, :db
+attr_reader :id
+
+def initialize(name)
+  @id = nil
+  @type = nil
+  @db = nil
+  @name = name
+end
+
+def self.save(name, type, db)
+end
+
+def self.find(id)
+end
+
+
+
+
 end
