b6f539aa4e70529e7120f8cfe82550b02893bbb6
Bernadette Davis <bsd231@nyu.edu>
2019-08-30 21:34:08 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,13 +2,13 @@ class Pokemon
   attr_accessor :name, :type, :db
   attr_reader :id
 
-  def initialize(name, type, id)
+  def initialize(name, type, id, db)
 
 
     @name = name
     @type = type
     @id = id
-    #@db = db
+    @db = db
 
   end
 
