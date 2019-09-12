2a11883f9a9753f7ae9a95e201f5d8ca1396c9c8
Connor Cowan <ccowan003@comcast.net>
2019-09-11 17:31:06 -0700

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,23 +1,20 @@
 class Pokemon
-  attr_accessor :name, :type, :db
-  attr_reader :id
+  attr_accessor :id, :name, :type, :db
 
-  def initialize(id:, name:, type:, db)
+  def initialize(id:, name:, type:, db:)      #keyword arugments
     @id = id
     @name = name
     @type = type
     @db = db
   end
-  def self.save
-    if self.id
-      self.update
-    else
-      sql = <<-SQL
-        INSERT INTO students (name, grade)
-        VALUES (?, ?)
-      SQL
-        DB[:conn].execute(sql, self.name, self.grade)
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-    end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(num, db)
+    poke_info = db.execute("SELECT * FROM pokemon WHERE id=?", num).first
+    #Pokemon.new(pokemon_info, db)
+    Pokemon.new(id: poke_info[0], name: poke_info[1], type: poke_info[2], db: db)
   end
 end
