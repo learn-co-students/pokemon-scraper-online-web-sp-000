aa25f8371270e8c77b0d465c869c40ecd767ed78
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-09-19 18:36:07 +0000

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -5,3 +5,36 @@ Scraper.new(@db).scrape
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
+
+# def milo
+#   @db = SQLite3::Database.new('../pokemon.db')
+#   @db.execute("DROP TABLE IF EXISTS pokemon")
+#   @sql_runner = SQLRunner.new(@db)
+#   @sql_runner.execute_schema_migration_sql
+#   Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)
+#   Pokemon.save("Pikachu", "electric", @db)
+#   loki = @db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
+#   loki
+# end
+# answer to "milo" should be [[1, "Pikachu", "electric"]]
+#
+#
+#   @db = SQLite3::Database.new('../pokemon.db')
+#   @db.execute("DROP TABLE IF EXISTS pokemon")
+#   @sql_runner = SQLRunner.new(@db)
+#   @sql_runner.execute_schema_migration_sql
+#   Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)
+#   Pokemon.save("Pikachu", "electric", @db)
+#   sql = "SELECT * FROM pokemon WHERE id = ? LIMIT 1"
+#
+#   @db = SQLite3::Database.new('../pokemon.db')
+#   @db.execute("DROP TABLE IF EXISTS pokemon")
+#   @sql_runner = SQLRunner.new(@db)
+#   @sql_runner.execute_schema_migration_sql
+#   Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)
+#   Pokemon.save("Pikachu", "electric", @db)
+#      sql = "SELECT * FROM pokemon WHERE id = ?"
+#      don = @db.execute(sql, 1).flatten
+#
+# binding.pry
+# 0
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,39 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?);", name, type)
+    @id = db.execute('SELECT last_insert_rowid() FROM pokemon')[0][0]
+  end
+
+  # def self.save(name, type, db)
+  #   sql = <<-SQL
+  #   INSERT INTO pokemon (name, type)
+  #   VALUES (?, ?)
+  #   SQL
+  #
+  #   db.execute(sql, name, type)
+  #   @id = db.execute('SELECT last_insert_rowid() FROM pokemon')[0][0]
+  # end
+
+  def self.find(id, db)
+      row = db.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
+      Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+  end
+
+  # def self.find(id, db)
+  #    sql = "SELECT * FROM pokemon WHERE id = ?"
+  #    db.execute(sql, id).map do |row|
+  #      Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+  #    end.first
+  # end
+
 end
