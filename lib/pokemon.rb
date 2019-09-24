8e00d035a04087943b7fb52e46b555ee3deda713
Austin Asbury <acasbury25@aol.com>
2019-09-24 17:12:45 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize (pokehash)
+    @id = pokehash[:id]
+    @name = pokehash[:name]
+    @type = pokehash[:type]
+    @db = pokehash[:db]
+  end
+
+  def self.save (name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+    SQL
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    db.execute("SELECT * FROM pokemon WHERE id = ?", id).map do |row|
+      self.new({:id => row[0], :name => row[1], :type => row[2], :db => db})
+    end.first
+  end
 end
