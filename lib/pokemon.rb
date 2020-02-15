b875411c9d46442a8ab349c819461cd4a641c0ba
CoderJay06 <jaycru06@yahoo.com>
2020-02-14 21:25:22 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,15 +10,11 @@ class Pokemon
     end 
 
     def self.save(name, type, db) 
-        # saves an instance of a pokemon with the correct id 
-        #binding.pry 
         db.execute("INSERT INTO pokemon (name, type) 
                     VALUES (?, ?)", [name, type])
     end 
 
-    def self.find(id, db)
-        # finds a pokemon from the database by their id number and returns a new Pokemon object 
-       # binding.pry  
+    def self.find(id, db) 
         pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", [id]).flatten
         Pokemon.new(id: pokemon[0],name: pokemon[1],type: pokemon[2],db: db)
     end 
