c59f37f9bd0709f9e82afdd1788e8f06aba4b33d
Grayson <graysonpberger@gmail.com>
2020-04-12 12:30:24 -0400

    Revert "Done."
    This reverts commit 1904bee2a43388ab26c75d52349c3640e86faf96.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,24 +1,2 @@
 class Pokemon
-    
-    attr_accessor :id, :name, :type, :db
-
-    def initialize(id: nil, name:, type:, db:)
-        @id = id
-        @name = name
-        @type = type
-        @db = db
-    end
-
-    def self.save(name, type, db)
-        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type).flatten.pop
-    end
-    
-    def self.find(id_num, db)
-    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
-    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
-    end
-
-    def alter_hp(new_hp, db)
-        db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
-    end
 end
