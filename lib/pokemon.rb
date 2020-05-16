f1a533d5769e847c20b3f4ae95245a99c55c01a7
dsulliv4 <dsulliv4@binghamton.edu>
2020-05-16 01:02:19 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,9 @@
 class Pokemon
-end
+    attr_accessor: :id, :name, :type, :db)
+
+    def initialize (id=nil, name:, type:, db:)
+        @name = name
+        @type = type
+        @db = db
+    end 
+end 
