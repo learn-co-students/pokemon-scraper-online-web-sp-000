d3b657979c7c5fce6d76bace58f4534e1b1091e8
gabejoseph <gabriel.joseph4@gmail.com>
2020-05-24 19:23:29 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,68 +16,14 @@ class Pokemon
     end
 
     def self.find(id, db)
-        found_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+        sql = <<-SQL
+            SELECT *
+            FROM pokemon 
+            WHERE id = ? 
+        SQL
+
+        found_pokemon = DB[:conn].execute(sql, id)
         Pokemon.new(id: found_pokemon[0], name: found_pokemon[1], type: found_pokemon[2], db: db)
     end
-
-    # def self.new_from_db(row)
-    #     new_student = self.new(row[1], row[2], row[0])
-    # end 
-
-    # def self.all
-    #     sql = <<-SQL
-    #         SELECT *
-    #         FROM students
-    #     SQL
-        
-    #     DB[:conn].execute(sql).map do |row|
-    #         self.new_from_db(row)
-    #     end 
-    # end
-
-    # def self.find_by_name(name)
-    #     sql = <<-SQL
-    #         SELECT *
-    #         FROM students
-    #         WHERE name = ?
-    #         LIMIT 1
-    #     SQL
-        
-    #     DB[:conn].execute(sql, name).map do |row|
-    #         self.new_from_db(row)
-    #     end.first
-    # end
-
-    # def self.create(name, grade)
-    #     student = Student.new(name, grade)
-    #     student.save
-    #     student
-    # end
-
-    # def update
-    #     sql = <<-SQL 
-    #         UPDATE students 
-    #         SET name = ?, grade = ? 
-    #         WHERE id = ?
-    #         SQL
-    #     DB[:conn].execute(sql, self.name, self.grade, self.id)
-    # end 
-    
-    # def self.create_table
-    #     sql = <<-SQL
-    #     CREATE TABLE IF NOT EXISTS students (
-    #         id INTEGER PRIMARY KEY,
-    #         name TEXT,
-    #         grade TEXT
-    #     )
-    #     SQL
-
-    #     DB[:conn].execute(sql)
-    # end
-
-    # def self.drop_table
-    #     sql = "DROP TABLE IF EXISTS students"
-    #     DB[:conn].execute(sql)
-    # end
 
 end
