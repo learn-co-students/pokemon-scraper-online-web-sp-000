87602bb7ceb4e2ae2ab9c19c6bfd6ffad1c328ae
Erin Stansbury <ekstansbury@gmail.com>
2020-06-01 13:01:20 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,39 +1,26 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+    @@all = []
 
-    attr_accessor :id, :name, :type
-    attr_reader :db
+  def initialize (id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
 
-    def initialize(pokemon)
-        @name = pokemon[:name]
-        @type = pokemon[:type]
-        @id = pokemon[:id]
-        @db = pokemon[:db]
-    end
+  def self.all
+    @@all
+  end
 
-    def self.save(name, type, db)
-        sql = <<-SQL
-        INSERT into pokemon (name, type)
-        VALUES (?,?)
-        SQL
-        db.execute(sql, name, type)
-    end
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
 
-    def self.find(id_number, db)
-        sql = <<-SQL 
-        select * FROM pokemon WHERE id = ?
-        SQL
-        db.execute(sql,id_number).map do |row| self.new_from_db(row)
-        end.first
-    end
+  def self.find (id, db)
+      pokemon = db.execute("SELECT * FROM pokemon WHERE id =?", id).flatten
 
-    def self.new_from_db(row)
-        puts row.class
-        puts row[0]
-        puts row[1]
-        puts row[2]
-
-        new_pokemon = self.new({id:row[0], name:row[1], type:row[2]});
-        new_pokemon
-    end
-
+      self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+  end
 end
