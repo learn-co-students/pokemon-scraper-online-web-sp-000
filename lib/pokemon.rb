3c403a7962f52bb1c42b84acc47632c95feb756f
jake0988 <jake098877@gmail.com>
2020-06-12 15:38:14 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(name:, type:, db:, id:nil)
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save
+        sqlite3 ./db/pokemon.db < ./db/schema_migration.sql
+        DB[:conn].execute(sql, self.name, self.type, self.db)
+        @id = DB[:conn].execute("SELECT last insert_row() FROM pokemon")[0][0]
+    end
 end
