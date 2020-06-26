3504b970bef01655cb3368cc04b0ae633cb5edf2
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-26 00:42:05 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(id:, name: , type:, db:)
+    @id = nil
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type, db)
+      VALUES (?,?)
+    SQL
+
+    db.execute(sql, self.name, self.type)
+  end
+
+  # def self.find
+  #   sql = <<-SQL
+  #     SELECT *
+  #     FROM students
+  #     WHERE id = ?
+  #   SQL
+  # end
 end
