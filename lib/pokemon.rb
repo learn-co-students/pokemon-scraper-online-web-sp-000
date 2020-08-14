3fdb265f172488b787d4f0ea6bf39600e85a7f60
Ian Mayton <ian.mayton@gmail.com>
2020-08-14 20:59:52 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,3 +1,12 @@
 class Pokemon 
+  attr_accessor :name, :type, :db, :id 
+  
+  def initialize(id: nil, name: name, type: type, db: db)
+    @id = id 
+    @name = name 
+    @type = type 
+    @db = db 
+  end 
+  
   
 end 
