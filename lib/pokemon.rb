884641e49d4e0a84108afdf14d55d3dc1bebd877
reevesalic <reevesalic@gmail.com>
2020-08-26 16:45:38 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+
+     attr_accessor :name, :id, :type, :db
+
+     def initialize(name, id, type, db)
+          @name = name
+          @id = id
+          @type = type
+          @db = db
+     end
+
+     def self.save
+
+     end
+
 end
