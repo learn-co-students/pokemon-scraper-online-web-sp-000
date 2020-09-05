b93ebfe3e774fb577b1bed11c42bff50bcd3be62
javonesm89 <javosmith12@gmail.com>
2020-09-05 12:17:01 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,12 @@
 require_relative "environment"
+require_relative "../lib/pokemon"
 
 Scraper.new(@db).scrape
 
-all_pokemon = @db.execute("SELECT * FROM pokemon;")
+# all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
+
+
+
+@db.execute("SELECT * FROM pokemon WHERE id = 4;").map {|pokemon| puts pokemon }
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,34 @@
 class Pokemon
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(name:,type:,db:nil,id:)
+        @name = name
+        @type = type
+        @db = db
+        @id = id
+    end
+
+    def self.save(name,type,db)
+        sql_save = <<-SQL
+        INSERT INTO pokemon(name,type)
+        VALUES (?,?)
+        SQL
+        db.execute(sql_save,name,type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.create_from_db(row)
+        pokemon = Pokemon.new(name: row[1], id: row[0], type: row[2])
+        pokemon
+    end
+    
+    def self.find(id,db)
+        sql_find = <<-SQL
+        SELECT * FROM pokemon
+        WHERE id = ?
+        SQL
+        db.execute(sql_find,id).map {|pokemon| self.create_from_db(pokemon)}.first
+    end
+
 end
