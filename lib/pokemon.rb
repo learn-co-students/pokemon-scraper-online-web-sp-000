223a37d200f4b1d40ad376d1f386184023d257da
Drew Thompson <drewallynthompson@gmail.com>
2020-10-05 03:04:25 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
 class Pokemon
+  
+  attr_accessor :name, :type, :db 
+  attr_reader :id 
+  
+  def initialize(id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db 
+  end
+  
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon
+      (name, type) VALUES (?,?)
+    SQL
+    
+    db.execute(sql, name, type)
+  end
+  
+  def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE id = ?"
+    pokemon = db.execute(sql, id)[0]
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    binding.pry
+  end
 end
