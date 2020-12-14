3b38e500f7f494d57e18e8032837ffa4147f5479
deliaconstantino <dconstan92@gmail.com>
2020-12-14 12:06:16 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,6 +26,6 @@ class Pokemon
     SQL
 
     row = db.execute(sql, id).first
-    # Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+    Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
   end
 end
