57ac22028fece320fa1fd74691f0d841d94576b2
carolyn-manning <carolyn.manning3@gmail.com>
2021-02-03 20:33:48 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,14 +1,14 @@
 class Pokemon
 
-    attr_accessor :name, :type, :db, :hp
+    attr_accessor :name, :type, :db
     attr_reader :id
 
-    def initialize(id:, name:, type:, db:, hp: nil)
+    def initialize(id:, name:, type:, db:)
         @id = id
         @name = name 
         @type = type 
         @db = db
-        @hp = hp
+        #@hp = hp
     end
 
     def self.save(name, type, db)
