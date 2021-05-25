444343ad3b25cb087df882700fe06d2404add976
“Daysha <+>
2021-05-25 14:34:04 -0700

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
+    end
+
+    def self.find(id, db)
+        result = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+        Pokemon.new(id: result[0][0], name: result[0][1], type: result[0][2], db: db)
+    end
 end
